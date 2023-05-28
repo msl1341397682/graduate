672810ce85b446e94ef6f6fca2cbcf8c0649cbea
msl <1341397682@qq.com>
2023-05-28 14:35:46 +0800

    这是我第一次给仓库提交自己的信息

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args){
+        //项目启动时，首先会调用类内部的公开的静态的返回类型为空的入口函数
+        int[] mylist=new int[10];
+        for(int i=0;i<10;i++){
+            mylist[i]=i;//初始化本数组
+        }
+        System.out.println(mylist.length);//直接获取数组的长度
+    }
+}
